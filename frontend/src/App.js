b661b5b696766a0e0eca1b7b982164675d032e4e
Jeewantha Hiddalarachchi <jeewhiddala@gmail.com>
2021-08-18 13:22:09 +0530

    Project footer added. AB#34. AB#37. AB#39

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';    //make routes
 import NavBar from './components/navBar/navBar';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <Router>
+                <NavBar/>
+                    <section>
+                        <Switch>
+                            
+
+                        </Switch>
+                    </section>
+                <Footer/>
+            </Router>
     </div>
   );
 }
