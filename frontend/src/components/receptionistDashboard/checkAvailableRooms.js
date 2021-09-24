06d23679af963849b38fcadad418a16c371bf9da
deshani0000 <it19051826@my.sliit.lk>
2021-09-24 18:52:16 +0530

    update available rooms details. AB#101. AB#138

--- frontend/src/components/receptionistDashboard/checkAvailableRooms.js
+++ frontend/src/components/receptionistDashboard/checkAvailableRooms.js
@@ -1,10 +1,9 @@
 import './checkAvailableRooms.css';
-// import Swal from "sweetalert2";
 import React, { Component } from 'react';
 import axios from 'axios';
-// import CheckoutSteps from '../checkoutSteps/checkoutSteps'
 import './dash.css';
 import ReactPaginate from 'react-paginate';
+
 
 
 class CheckAvailableRooms extends Component {
@@ -13,33 +12,42 @@ class CheckAvailableRooms extends Component {
         this.state = {
             totalPages: 0,
             page: 0,
+            id: '',
+            roomNo: '',
             room: [],
+            
+
 
         }
-       
+
         this.navigateBookingPage = this.navigateBookingPage.bind(this);
         this.handlePageChange = this.handlePageChange.bind(this);  //pagination
-        this.retriveAvailableRoomsPages = this.retriveAvailableRoomsPages.bind(this);     //pagination
+        this.onChange = this.onChange.bind(this);
+
+        this.retriveAvailableRoomsPages = this.retriveAvailableRoomsPages.bind(this); //pagination
+
     }
     componentDidMount() {
         axios.get('http://localhost:8100/room/availableRooms')
             .then(response => {
                 this.setState({ room: response.data.data });
-                this.setState({ room: response.data.data.docs });          //pagination
-                this.setState({ totalPages: response.data.data.totalPages });          //pagination
+                this.setState({ room: response.data.data.docs });     //pagination
+                this.setState({ totalPages: response.data.data.totalPages });   //pagination
             })
 
     }
 
+
+
     navigateBookingPage(e, roomId) {
-        // window.location = `/createBooking`
         this.props.history.push({
             pathname: `/createBooking/${roomId}`,
             data: `${roomId}`
         });
     }
 
-    retriveAvailableRoomsPages(page) {               //pagination
+
+    retriveAvailableRoomsPages(page) {  //pagination
         console.log("Pagef", page);
         axios.get('http://localhost:8100/room/availableRooms', {
             params: {
@@ -52,32 +60,40 @@ class CheckAvailableRooms extends Component {
             })
     };
 
-    handlePageChange = (data) => {          //pagination
+    handlePageChange = (data) => {    //pagination
         let selected = data.selected + 1;
         console.log("val", selected);
         this.setState({ page: selected });
         this.retriveAvailableRoomsPages(selected);
     };
 
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+        this.setState({ isAvailable: false });
+    }
+
+
 
     render() {
         return (
 
             <div>
-                <br></br>
-                <br></br>
-                <br></br>
+                <br /><br />
+
+
+
                 <div className="row justify-content-center" id="dash-box">
                     <div className="container-dash">
                         <h2><b>Receptionist Dashboard</b></h2>
                         <div className="row justify-content-evenly">
-                            <div className="col-3">
+                            <div className="col-3 align-self-stretch">
 
                                 <div className="row">
                                     <div className="container" >
                                         <h3 className="h3"><b>Creations</b></h3>
                                         <div className="list-group">
                                             <a href="/checkAvailableRooms" id="active-button" className="routeBtn"><button type="button" className="list-group-item list-group-item-action active" aria-current="true">Check Available Rooms</button></a>
+                                            <a href="/checkUnAvailableRooms" id="active-button" className="routeBtn"><button type="button" className="list-group-item list-group-item-action">Check UnAvailable Rooms</button></a>
                                             <a href="/roomBookingManagement" className="routeBtn"><button type="button" className="list-group-item list-group-item-action " >
                                                 Room Booking Management
                                             </button></a>
@@ -89,23 +105,25 @@ class CheckAvailableRooms extends Component {
                                         </div>
                                     </div>
                                 </div>
-                                <br /><br /><br /><br />
-                                <br></br>
-                                <br></br>
-                                <br></br>
-                                <br></br>
+
 
                             </div>
-                            <div className="col-8">
+                            <div className="col-8 align-self-stretch">
                                 <div className="container" >
 
 
+
+
                                     <div className="float-end">
-                                        <form className="d-flex">
-                                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                        <form className="d-flex" >
+                                          
                                             <button className="btn btn-primary" type="submit">Search</button>
                                         </form>
+
                                     </div>
+
+
+
                                     <div className="col-4">
                                         <h3 className="h3"><b>Check Available Rooms</b></h3>
                                     </div>
@@ -121,10 +139,8 @@ class CheckAvailableRooms extends Component {
                                                     <th scope="col">Category</th>
                                                     <th scope="col">A/C Category</th>
                                                     <th scope="col">Description</th>
-
                                                     <th scope="col">Price(Rs.)</th>
-
-                                                    <th>Status</th>
+                                                    <th>Status (isAvailable)</th>
                                                     <th></th>
 
 
@@ -139,15 +155,12 @@ class CheckAvailableRooms extends Component {
                                                         <td>{item.category}</td>
                                                         <td>{item.airConditioningCategory}</td>
                                                         <td>{item.description}</td>
-
                                                         <td>{item.price}</td>
+                                                        <td> Available ({String(item.isAvailable)})
 
-
-                                                        <td><button type="button" className="btn btn-default" onClick={e => this.navigat(e, item._id)}
-
-
-                                                        >Available</button></td>
-                                                        <td><button type="button" className="btn btn-success" onClick={e => this.navigateBookingPage(e, item._id)}  >Book</button></td>
+                                                        </td>
+                                                        <td><button type="button" className="btn btn-success" onClick={e => this.navigateBookingPage(e, item._id)}  >Book</button>
+                                                        </td>
 
 
                                                     </tr>
@@ -172,10 +185,8 @@ class CheckAvailableRooms extends Component {
                         </div>
                     </div>
                 </div>
+                <br /><br />
 
-
-                <br /><br /><br /><br />
-                <br /><br /><br /><br />
             </div>
 
         )
