5b5224a0073b26e5b15c034014ea9716c526e9e5
anjanadeseram987 <it19021058@my.sliit.lk>
2021-09-26 14:46:20 +0530

    backend update, search food Management. AB#103. AB#150

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -53,9 +53,33 @@ const deleteFood = async (req, res) => {               // delete selected food.
     }
 }
 
+const updateSelectedFoodDetails = async (req, res) => {       //update selected food
+    if (req.params && req.params.id){
+        const {id} = req.params;        // fetching the id of the food.
+        const food = req.body;
+
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Food With that id');      // validating the food id
+        const updatedFood = await Food.findByIdAndUpdate(id, food,{new : true});      // find food 
+        res.json(updatedFood);
+    }
+}
+
+const getSearchedFoodDetailsByNo = async (req, res) => {          //get selected food details. //search
+    var foodNumber = req.params.foodNumber;
+    await Food.findOne({foodNumber: foodNumber}).populate('chefName','name')
+        .then(data => {
+            res.status(200).send({ data: data });
+        })
+        .catch(error => {
+            res.status(500).send({ error: error.message });
+        });
+}
+
 module.exports = {
     createFood,
     getAllFoodsDetails,
     getSelectedFoodDetails,
-    deleteFood
+    updateSelectedFoodDetails,
+    deleteFood,
+    getSearchedFoodDetailsByNo
 };
