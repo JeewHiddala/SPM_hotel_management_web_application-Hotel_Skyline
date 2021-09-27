6dd52369586186ec3b9ea6ebb3c064fc8b7b64d0
anjanadeseram987 <it19021058@my.sliit.lk>
2021-09-28 01:15:18 +0530

    Modify create Ingredient form. AB#105. AB#147

--- frontend/src/components/kitchenHeadComponents/createForms/ingredient/ingredient.js
+++ frontend/src/components/kitchenHeadComponents/createForms/ingredient/ingredient.js
@@ -107,7 +107,7 @@ class Ingredient extends Component {
         const { selectedChef } = this.state.selectedChef;
 
         return (
-            <div className="row justify-content-center">
+            <div className="row justify-content-center" id="dash-food">
                 <div className="container-dash">
                     <h2><b>Kitchen Head Dashboard</b></h2>
                     <div className="row justify-content-evenly">
