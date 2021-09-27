63967b80ee5a6cc87861f8fe68c9b335a84dd9c3
anjanadeseram987 <it19021058@my.sliit.lk>
2021-09-28 01:15:48 +0530

    Modify create Ingredient order form. AB#105. AB#147

--- frontend/src/components/kitchenHeadComponents/createForms/ingredientOrder/ingredientOrder.js
+++ frontend/src/components/kitchenHeadComponents/createForms/ingredientOrder/ingredientOrder.js
@@ -45,7 +45,7 @@ class IngredientOrder extends Component {
     render() {
 
         return (
-            <div className="row justify-content-center">
+            <div className="row justify-content-center" id="dash-food">
                 <div className="container-dash">
                     <h2><b>Kitchen Head Dashboard</b></h2>
                     <div className="row justify-content-evenly">
