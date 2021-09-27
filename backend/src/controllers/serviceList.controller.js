778993bf4b5e211931019a2488dff1fd4cd57f98
anjanadeseram987 <it19021058@my.sliit.lk>
2021-09-28 03:14:45 +0530

    re-modify Service List. AB#104. AB#148

--- backend/src/controllers/serviceList.controller.js
+++ backend/src/controllers/serviceList.controller.js
@@ -15,16 +15,6 @@ const createServiceList = async (req, res) => {       //create a ServiceList to 
 }
 
 const getAllServiceListsDetails = async (req, res) => {       //get all ServiceList details.
-    // let page = req.query.page; 
-    // var abc = ({ path: 'customerServices', select: 'date noOfHours price cost' });
-    // var abcde = ({ path: 'serviceName', select: 'name' });
-    // const options = {
-    //     page: page,
-    //     populate: abc,
-    //     populate: abcde,
-    //     limit: 5
-    //   }
-    //   console.log("Page", req.query.page);  
     await ServiceList.find({}).populate('customerServices','serviceName date noOfHours price cost')
     .populate('bookingID','bookingNo')
     .populate({
@@ -79,28 +69,10 @@ const updateSelectedServiceListDetails = async (req, res) => {       //update se
     }
 }
 
-// const getSearchedServiceListDetailsByNo = async (req, res) => {          //get selected service list details. //search
-//     var bookingID = req.params.bookingID;
-//     await ServiceList.findOne({bookingID: bookingID}).populate('customerServices','serviceName date noOfHours price cost')
-//     .populate('bookingID','bookingNo') 
-//     .populate({
-//         path: 'customerServices',
-//         populate: {
-//             path: 'serviceName'
-//         }
-//     }).then(data => {
-//             res.status(200).send({ data: data });
-//         })
-//         .catch(error => {
-//             res.status(500).send({ error: error.message });
-//         });
-// }
-
 module.exports = {
     createServiceList,
     getAllServiceListsDetails,
     getSelectedServiceListDetails,
-    //getSearchedServiceListDetailsByNo,
     updateSelectedServiceListDetails,
     deleteServiceList
 };
