f51c805dac4a05941ff131b5cb874227cd4cc96f
Jeewantha Hiddalarachchi <jeewhiddala@gmail.com>
2021-09-30 04:50:48 +0530

    modifing test cases.

--- backend/src/test/systemBackendTesting.test.js
+++ backend/src/test/systemBackendTesting.test.js
@@ -6,16 +6,12 @@ const bodyParser = require('body-parser');
 const request = require('supertest');
 
 //import APIs
-const attendanceAPI = require('../apis/attendance.api');//IT19059150
+const attendanceAPI = require('../apis/attendance.api');//IT19059150 - Ranaweera I.G.S.V
 const employeeLeaveAPI = require('../apis/employeeLeaves.api');//IT19059150
-
 const foodApi = require('../apis/food.api');// IT19021058 -De Seram E.M.A.P.
- const ingredientApi = require('../apis/ingredient.api'); // IT19021058 -De Seram E.M.A.P.
- const serviceListApi = require('../apis/serviceList.api'); // IT19021058 -De Seram E.M.A.P.
+const ingredientApi = require('../apis/ingredient.api'); // IT19021058 -De Seram E.M.A.P.
+const serviceListApi = require('../apis/serviceList.api'); // IT19021058 -De Seram E.M.A.P.
 const customerServiceApi = require('../apis/customerService.api'); // IT19021058 -De Seram E.M.A.P.
-
-
-//IT19007502 - Hiddalarachchi J.
 const roomAPI = require('../apis/room.api'); //IT19007502 - Hiddalarachchi J.
 const serviceAPI = require('../apis/service.api');  //IT19007502 - Hiddalarachchi J.
 const employeeAPI = require('../apis/employee.api');    //IT19007502 - Hiddalarachchi J.
@@ -57,17 +53,18 @@ app.listen(PORT, () => {
     console.log(`Server is up and running on PORT ${PORT}`);
 });
 
-//register router - CHANGEABLE
-app.use('/attendance', attendanceAPI());
-app.use('/employeeLeaves', employeeLeaveAPI());
-
-
 //register router - CHANGEABLE.
 //IT19007502 - Hiddalarachchi J.
 app.use('/room', roomAPI());    //IT19007502 - Hiddalarachchi J.
 app.use('/employee', employeeAPI());    //IT19007502 - Hiddalarachchi J.
 app.use('/service', serviceAPI());  //IT19007502 - Hiddalarachchi J.
-app.use('/booking', bookingAPI()); //IT19051826
+app.use('/booking', bookingAPI()); //IT19051826 - Herath D.D.M
+app.use('/food', foodApi());// IT19021058 -De Seram E.M.A.P.
+app.use('/serviceList', serviceListApi());  // IT19021058 -De Seram E.M.A.P.
+app.use('/ingredient', ingredientApi()); // IT19021058 -De Seram E.M.A.P.
+app.use('/customerService', customerServiceApi());  // IT19021058 -De Seram E.M.A.P.
+app.use('/attendance', attendanceAPI());
+app.use('/employeeLeaves', employeeLeaveAPI());
 
 //TestCases
 //test case 01 - add new room
@@ -78,15 +75,6 @@ test('Backend Test Case 01 - Should insert a new room - IT19007502 - Hiddalarach
         airConditioningCategory:"A/C",
         description: "High Budget Room",
         price:18000,
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
-
-//test case 2 IT19059150
-test('Backend Test Case 25 - Should get all employee Leaves details - IT19059150  - Ranaweera I.G.S.V.', async () => {
-    await request(app).get('/employeeLeaves/').send({  
-
     }).expect(200).then((res) => {
         id = res.body._id;
     });
@@ -243,7 +231,6 @@ test('Backend Test Case 14 - Should get all booking details - IT19051826  - Hera
 
 //IT19051826 Test Case 15
 //test case 15 - create booking 
-
 test('Backend Test Case 15 - Should create booking - IT19051826  - Herath D.D.M.', async () => {
     await request(app).post('/booking/create').send({  
         bookingNo:"B009",
@@ -272,88 +259,6 @@ test('Backend Test Case 16 - delete booking details - IT19051826  - Herath D.D.M
 
 //IT19051826 Test Case 17
 //test case 17 - Update selected booking details 
-
-app.use('/food', foodApi());// IT19021058 -De Seram E.M.A.P.
- app.use('/serviceList', serviceListApi());  // IT19021058 -De Seram E.M.A.P.
-app.use('/ingredient', ingredientApi()); // IT19021058 -De Seram E.M.A.P.
- app.use('/customerService', customerServiceApi());  // IT19021058 -De Seram E.M.A.P.
-
-
-//test case 18 - add new food
-test('should insert a new Food', async () => {
-    await request(app).post('/food/create').send({
-        foodNumber: "N00648",
-        foodName: "Rice",
-        category: "Dinner",
-        price: 500,
-        description: "Fried Rice",
-        createDate: "2021-08-10",
-        status: "True",
-        chefName: "416d616c6920706572657261",
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
-
-//test case 19 - update food details
-test('should update Food', async () => {
-    await request(app).patch('/food/update/6154b935a96a0b141c85bf68').send({
-        foodNumber: "N00648",
-        foodName: "Rice",
-        category: "Dinner",
-        price: 800,
-        description: "Fried Rice",
-        createDate: "2021-08-10",
-        status: "True",
-        chefName: "416d616c6920706572657261",
-    }).expect(200).then((res) => {
-
-    });
-})
-
-//test case 20 - delete selected ingredient
-test('should delete ingredient', async () => {
-    await request(app).delete('/ingredient/61249bda7512594d4c3c5c4a').send({
-
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
-
-//test case 21 - create new ingredient
-test('should insert a new Ingredient', async () => {
-    await request(app).post('/ingredient/create').send({
-        orderNumber: "IO00082",
-        ingredientName: "Carrot",
-        quantity: "15kg",
-        chefName: "611a0f6ca28bea02808efe8a",
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
-
-//test case 22 - get all service list details
-test('should get service list details', async () => {
-    await request(app).get('/serviceList').send({
-
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
-
-//test case 23 - add new customer service
-test('should insert a new Customer service', async () => {
-    await request(app).post('/customerService/create').send({
-        bookingID: "61267e14d326d6217cc60b11",
-        serviceName: "6124244653d5e665c865323e",
-        date: "2021-09-28",
-        noOfHours: 2,
-        price: 750,
-        cost: 1500,
-    }).expect(200).then((res) => {
-        id = res.body._id;
-    });
-})
 test('Backend Test Case 17 - should update selected booking details - IT19051826  - Herath D.D.M.', async () => {
     await request(app).patch('/booking/update/6123ec1ca2b1e110bc563b67').send({  
         bookingNo:"B009",
@@ -370,7 +275,83 @@ test('Backend Test Case 17 - should update selected booking details - IT19051826
     });
 })
 
-//test case 1 IT19059150
+//test case 18 - add new food
+test('Backend Test Case 18 - should insert a new Food - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).post('/food/create').send({
+        foodNumber: "N00648",
+        foodName: "Rice",
+        category: "Dinner",
+        price: 500,
+        description: "Fried Rice",
+        createDate: "2021-08-10",
+        status: "True",
+        chefName: "416d616c6920706572657261",
+    }).expect(200).then((res) => {
+        id = res.body._id;
+    });
+})
+
+//test case 19 - update food details
+test('Backend Test Case 19 - should update Food - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).patch('/food/update/6154b935a96a0b141c85bf68').send({
+        foodNumber: "N00648",
+        foodName: "Rice",
+        category: "Dinner",
+        price: 800,
+        description: "Fried Rice",
+        createDate: "2021-08-10",
+        status: "True",
+        chefName: "416d616c6920706572657261",
+    }).expect(200).then((res) => {
+
+    });
+})
+
+//test case 20 - delete selected ingredient
+test('Backend Test Case 20 - should delete ingredient - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).delete('/ingredient/61249bda7512594d4c3c5c4a').send({
+
+    }).expect(200).then((res) => {
+        id = res.body._id;
+    });
+})
+
+//test case 21 - create new ingredient
+test('Backend Test Case 21 - should insert a new Ingredient - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).post('/ingredient/create').send({
+        orderNumber: "IO00082",
+        ingredientName: "Carrot",
+        quantity: "15kg",
+        chefName: "611a0f6ca28bea02808efe8a",
+    }).expect(200).then((res) => {
+        id = res.body._id;
+    });
+})
+
+//test case 22 - get all service list details
+test('Backend Test Case 22 - should get service list details - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).get('/serviceList').send({
+
+    }).expect(200).then((res) => {
+        id = res.body._id;
+    });
+})
+
+//test case 23 - add new customer service
+test('Backend Test Case 23 - should insert a new Customer service - IT19021058  - De Seram E.M.A.P.', async () => {
+    await request(app).post('/customerService/create').send({
+        bookingID: "61267e14d326d6217cc60b11",
+        serviceName: "6124244653d5e665c865323e",
+        date: "2021-09-28",
+        noOfHours: 2,
+        price: 750,
+        cost: 1500,
+    }).expect(200).then((res) => {
+        id = res.body._id;
+    });
+})
+
+//test case 24 IT19059150
 test('Backend Test Case 24 - Should enter a new attendance - IT19059150  - Ranaweera I.G.S.V.', async () => {
     await request(app).post('/attendance/create').send({
         "employee": "61202cba45591f2f9421f5c7",
@@ -378,5 +359,13 @@ test('Backend Test Case 24 - Should enter a new attendance - IT19059150  - Ranaw
         "status": "Working"
     }).expect(200).then((res) => {
       
+    });
+})
+
+//test case 25 IT19059150
+test('Backend Test Case 25 - Should get all employee Leaves details - IT19059150  - Ranaweera I.G.S.V.', async () => {
+    await request(app).get('/employeeLeaves/').send({  
+    }).expect(200).then((res) => {
+        id = res.body._id;
     });
 })
